df06422aa7fd9795066e7efad8ef8da8c61c9355
Joey Clover <joey.clover@gmail.com>
2020-12-19 23:33:40 +0000

    add bastion

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -16,14 +16,37 @@ resource "aws_db_instance" "scannerfleet_db" {
   storage_type           = "gp2"
   engine                 = "postgres"
   instance_class         = "db.t2.micro"
-  name                   = "ScannerFleetDBTechDemoDB"
+  name                   = "ScannerFleetDB"
   username               = var.pg_username
   password               = var.pg_password
   apply_immediately      = true
   db_subnet_group_name   = module.scannerfleet_vpc.subnet_group_id
-  vpc_security_group_ids = [module.scannerfleet_vpc.security_group_id]
+  vpc_security_group_ids = [module.scannerfleet_vpc.bastion_security_group_id]
   skip_final_snapshot    = true
-  publicly_accessible    = true
+  publicly_accessible    = false
+}
+
+data "aws_ami" "latest_ecs" {
+  most_recent = true
+  owners = ["591542846629"] # AWS
+
+  filter {
+      name   = "name"
+      values = ["*amazon-ecs-optimized"]
+  }
+
+  filter {
+      name   = "virtualization-type"
+      values = ["hvm"]
+  }  
+}
+
+resource "aws_instance" "bastion" {
+  ami                         = data.aws_ami.latest_ecs.id
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  vpc_security_group_ids = [module.scannerfleet_vpc.bastion_security_group_id]
+  subnet_id = module.scannerfleet_vpc.subnet1_id
 }
 
 
@@ -105,6 +128,10 @@ module "track_scan_service" {
   db_endpoint        = aws_db_instance.scannerfleet_db.endpoint
 }
 
+resource "aws_key_pair" "bastion_key" {
+  key_name   = "bastion_rsa_key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCcPrFBTePnqrZYeosiv+mZWeaVoVT16wgnCP8TR9bmJT6YNX7pdxpvFZn22WyC0+T7gZ48tVrBEcuYJYKUGiET2WlT/3+bjQWLcN47hoomiVhg9FqVuhxsxloRUNx/Qd2OxL+iFz7xTJklSvJbIa+6HR7lsdf0sfXimueoPhszmgWv7rdGuiByS3RJGkIy+0S27AJTXJl4nwWCL67Yl0Ddtd24hPTRNu2NgXn6xjJYonUv94qRur9hjtexOiEh3F7dfjOi5shgy82lI33hqmmP01h6rdiedgYzQhZ/mV3tUeCfTx6Uxo4ZFYjbMBW0hz1pt3Dtyw/Mfrqjg32T3d8qrz+UhDvLufhPTkXFi+sb3VveZaFKkHnRYq1fpfGpsUacU1AWdcmFMlqrUa0d01f+DfZZjeBhXArI8M9g/KI25Ebkq2w04dGDcPZU8WSs8x9TjN29xMX6zEgt6TMb87fcw8jl1yRJtn7q+nu0lO33XDlNLhl0aqxt6tyUoe51ukhEaKeWoCdG46ClLCzQqfKt5r5GkoLOWmUFvVn/RcYPliSjaBeJLkJ9nlR1QxGT1uGVv9ta+Gntnwv+/Xu4pvIHurEGPHmUX4ELvZ2bBE/j67c2faKTsc2jGW5GtLZmFQjw4AUoNF6K7sr5Jp3eC5u6jG9tqpsol9KzzZhKuERUeQ== joey.clover@gmail.com"
+}
 
 output "base_url" {
   value = aws_api_gateway_deployment.scanner_deployment.invoke_url
@@ -116,4 +143,8 @@ output "pg_endpoint" {
 
 output "s3_bucket" {
   value = module.lambda_s3_bucket.id
+}
+
+output "bastion_public_ip" {
+  value = aws_instance.bastion.public_ip
 }
--- infrastructure/modules/vpc/main.tf
+++ infrastructure/modules/vpc/main.tf
@@ -1,6 +1,5 @@
 resource "aws_vpc" "default" {
   cidr_block = "10.0.0.0/16"
-  enable_dns_hostnames = true
 }
 
 resource "aws_internet_gateway" "gw" {
@@ -11,29 +10,36 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
+resource "aws_security_group" "bastion_sg" {
+  name   = "bastion-security-group"
+  vpc_id = aws_vpc.default.id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = -1
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_security_group" "default" {
   name        = "scannerfleetsc"
-  description = "Security group for AWS lambda and AWS RDS connection"
+  description = "Security group for vpc"
   vpc_id      = aws_vpc.default.id
+
   ingress {
     from_port       = 0
     to_port         = 0
     protocol        = "-1"
     cidr_blocks     = ["127.0.0.1/32"]
     self = true
-  }
-  ingress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["5.81.206.174/32"]
-    self = true
-  }
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["5.81.206.174/32"]
   }
   egress {
     from_port       = 0
